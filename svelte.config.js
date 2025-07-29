647e6ebdf715ba7c2e2957dfc47e2ae03ce414fb
Adrian Benito <adrian.benito.carbo@gmail.com>
2025-07-29 18:45:27 +0200

    feat

--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,21 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter(),
-		alias: {
-			'@assets/*': './src/assets/*'
-		},
-		paths: {
-			base: dev ? '' : '/happy-birthday'
-		},
-		prerender: {
-			entries: ['*']
-		}
-	}
+
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: dev ? '' : '/happy-birthday'
+    },
+    prerender: {
+      entries: ['*']
+    }
+  }
 };
 
-export default config;
