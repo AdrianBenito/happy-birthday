02d0fa646e656aff67275d46b548309a70bf1ea0
Adrian Benito <adrian.benito.carbo@gmail.com>
2025-07-29 19:11:01 +0200

    Add .nojekyll to avoid Jekyll processing

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
-
-export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    paths: {
-      base: dev ? '' : '/happy-birthday'
-    },
-    prerender: {
-      entries: ['*']
-    }
-  }
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
+export default config;
